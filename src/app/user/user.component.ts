10f1a03df5f33cfacc550c72893504aec76621fa
Kripa Sindhu <sindhukripa007@gmail.com>
2025-04-16 15:46:47 +0530

    day-3 afternoon

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,19 @@
-import { Component, computed, Input, input} from '@angular/core';
-import  {DUMMY_USERS} from '../dummy-users';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  Output,
+  output
+} from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
+type User = {
+  id: string;
+  name: string;
+  avatar: string;
+}
 
 @Component({
   selector: 'app-user',
@@ -9,10 +22,13 @@ import  {DUMMY_USERS} from '../dummy-users';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
-
 export class UserComponent {
-  @Input({required: true}) avatar!: string;
-  @Input({required: true}) name!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  @Input({required: true}) user!: User;
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>();
 
   // using signals
   // avatar = input.required<string>();
@@ -21,12 +37,12 @@ export class UserComponent {
   // imagePath = computed(() => {
   //   return 'assets/users/' + this.avatar();
   // });
- 
+
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-
+    this.select.emit(this.user.id);
   }
 }
