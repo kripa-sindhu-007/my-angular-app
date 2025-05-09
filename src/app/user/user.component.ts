77eeb3af411227ace5d3827acda0aee661266074
Kripa Sindhu <sindhukripa007@gmail.com>
2025-05-09 13:17:50 +0530

    migrated from standalone components to module based components

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,27 +12,14 @@ import { CardComponent } from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
-  standalone: true,
-  imports: [CardComponent],
+  standalone: false,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
-  // @Input({ required: true }) id!: string;
   @Input({required: true}) user!: User;
   @Input({required: true}) selectedUser!: boolean;
   @Output() select = new EventEmitter<string>();
-  // select = output<string>();
-
-  // using signals
-  // avatar = input.required<string>();
-  // name = input.required<string>();
-
-  // imagePath = computed(() => {
-  //   return 'assets/users/' + this.avatar();
-  // });
 
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
